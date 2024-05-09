4ce0e3530e2ad119f1316cf81656b8f600032d51
Abhinav Rau <abhinavrau>
2024-05-09 12:05:31 -0400

    use gemini 001 model

--- test/test_velvet_tables.js
+++ test/test_velvet_tables.js
@@ -123,12 +123,12 @@ describe("create Test Template Tables", () => {
             ["maxExtractiveSegmentCount (1-5)", "0"], //maxExtractiveSegmentCount
             ["maxSnippetCount (1-5)", "0"], //maxSnippetCount
             ["Preamble (Customized Summaries)", ""],
-            ["Summarization Model", "gemini-1.0-pro-002/answer_gen/v1"],
+            ["Summarization Model", "gemini-1.0-pro-001/answer_gen/v1"],
             ["summaryResultCount (1-5)", "2"],   //summaryResultCount
             ["useSemanticChunks (True or False)", "False"],   //useSemanticChunks
             ["ignoreAdversarialQuery (True or False)", "True"], // ignoreAdversarialQuery
             ["ignoreNonSummarySeekingQuery (True or False)", "True"], // ignoreNonSummarySeekingQuery
-            ["SummaryMatchingAdditionalPrompt", "If there are monetory numbers in the answers, they should be matched exactly."],
+            ["SummaryMatchingAdditionalPrompt", "If there are monetary numbers in the answers, they should be matched exactly."],
             ["Batch Size (1-10)", "2"], // BatchSize
             ["Time between Batches in Seconds (1-10)", "2"], // BatchSize
         ]);
